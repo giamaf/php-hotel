473d7548847d15ab59940d45a39a91f4b7f14b3e
giamaf <gianluca.maffucci@hotmail.com>
2024-02-20 19:40:28 +0100

    printed data + added readme

--- index.php
+++ index.php
@@ -3,17 +3,59 @@ include 'data/hotels_data.php';
 
 // Label pagina
 $page_label = 'PHP Hotel';
+
 ?>
 
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <!-- Bootstrap -->
+    <link rel='stylesheet' href='https://cdnjs.cloudflare.com/ajax/libs/bootstrap/5.3.3/css/bootstrap.min.css'
+        integrity='sha512-jnSuA4Ss2PkkikSOLtYs8BlYIeeIK1h99ty4YfvRPAlzr377vr3CXDb7sb7eEEBYjDtcYj+AjBH3FLv5uSJuXg=='
+        crossorigin='anonymous' />
+
     <title><?= $page_label ?></title>
 </head>
 
 <body>
+    <header class="text-center">
+        <h1 class="my-3"><?= $page_label ?></h1>
+    </header>
 
+    <main class="my-3">
+        <div class="container">
+            <table class="table">
+                <thead>
+                    <tr>
+                        <?php foreach ($hotels[0] as $title => $info ): ?>
+
+                        <th scope="col"><?= $title ?></th>
+
+                        <?php endforeach ?>
+                    </tr>
+                </thead>
+                <tbody>
+                    <?php foreach ($hotels as $hotel ): ?>
+
+                    <tr>
+                        <th scope="row"><?= $hotel['name'] ?></th>
+                        <td><?= $hotel['description'] ?></td>
+                        <td><?= $hotel['parking'] ?></td>
+                        <td><?= $hotel['vote'] ?></td>
+                        <td><?= $hotel['distance_to_center'] ?></td>
+                    </tr>
+
+
+                    <?php endforeach ?>
+
+                </tbody>
+            </table>
+        </div>
+    </main>
 </body>
+
 </html>
---
+++ readme.md
@@ -0,0 +1,15 @@
+# PHP Hotel
+
+Parto da questo array di hotel https://www.codepile.net/pile/OEWY7Q1G.<br>
+Stampo tutti gli hotels con tutti i dati disponibili.
+
+---
+
+### Bonus: <br>
+1 - Aggiungere un form ad inizio pagina che tramite una richiesta GET permetta di filtrare gli hotels che hanno un parcheggio.<br>
+2 - Aggiungere un secondo campo al form che permetta di filtrare gli hotels per voto (es. inserisco 3 ed ottengo tutti gli hotel che hanno un voto di tre stelle o superiore).<br>
+
+---
+
+<b>NOTA</b>: deve essere possibile utilizzare entrambi i filtri contemporaneamente (es. ottenere una lista con hotel che dispongono di parcheggio e che hanno un voto di tre stelle o superiore)
+Se non viene specificato nessun filtro, visualizzare come in precedenza tutti gli hotels.
