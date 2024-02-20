8a794ff13d1de8c937b0753044a8dca5558b295e
giamaf <gianluca.maffucci@hotmail.com>
2024-02-20 16:25:40 +0100

    first commit

---
+++ index.php
@@ -0,0 +1,16 @@
+<?php
+// Label pagina
+$page_label = 'PHP Hotel'
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title><?= $page_label ?></title>
+</head>
+<body>
+    
+</body>
+</html>
