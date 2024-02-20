5930e8f0079c0d54db93b14e724896101775b5c1
giamaf <gianluca.maffucci@hotmail.com>
2024-02-20 19:59:27 +0100

    added park icons

--- index.php
+++ index.php
@@ -18,6 +18,11 @@ $page_label = 'PHP Hotel';
         integrity='sha512-jnSuA4Ss2PkkikSOLtYs8BlYIeeIK1h99ty4YfvRPAlzr377vr3CXDb7sb7eEEBYjDtcYj+AjBH3FLv5uSJuXg=='
         crossorigin='anonymous' />
 
+    <!-- Fontawesome -->
+    <link rel='stylesheet' href='https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.css'
+        integrity='sha512-tx5+1LWHez1QiaXlAyDwzdBTfDjX07GMapQoFTS74wkcPMsI3So0KYmFe6EHZjI8+eSG0ljBlAQc3PQ5BTaZtQ=='
+        crossorigin='anonymous' />
+
     <title><?= $page_label ?></title>
 </head>
 
@@ -44,7 +49,12 @@ $page_label = 'PHP Hotel';
                     <tr>
                         <th scope="row"><?= $hotel['name'] ?></th>
                         <td><?= $hotel['description'] ?></td>
-                        <td><?= $hotel['parking'] ?></td>
+                        <?php if($hotel['parking']): ?>
+
+                        <td><i class="fa-regular fa-circle-check"></i></td>
+                        <?php else: ?>
+                        <td><i class="fa-regular fa-circle-xmark"></i></td>
+                        <?php endif ?>
                         <td><?= $hotel['vote'] ?></td>
                         <td><?= $hotel['distance_to_center'] ?></td>
                     </tr>
